57360956f796fec0d206bd08a03d8efb77d0cdf7
luislps <77980621+luislps@users.noreply.github.com>
2025-01-28 05:08:46 -0300

    commit changes 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -34,7 +34,7 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-        // impressão dos dados no terminal
+    // impressão dos dados no terminal
     printf("Nome do país: %s\n", nome);
     printf("População: %d\n", populacao);
     printf("Área: %d\n", area);
